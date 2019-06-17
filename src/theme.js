da80cc359e5e1e1c52497aed01802e758257b106
lol-russo <jack.lorusso@gmail.com>
2019-06-17 19:21:27 +1000

    Update theme object

--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,32 @@ const theme = {
     white: "#fff",
     text: "#37474f",
   },
-  fontSize: [14, 18, 24, 30, 36],
+  // Font sizes and scale units are calculated using a Perfect Fourth modular scale (1.333)
+  fontSize: [
+    "0.75em",
+    "1em",
+    "1.333em",
+    "1.777em",
+    "2.369em",
+    "3.157em",
+    "4.209em",
+  ],
+  scale: [
+    "0.317em",
+    "0.422em",
+    "0.563em",
+    "0.75em",
+    "1em",
+    "1.333em",
+    "1.777em",
+    "2.369em",
+    "3.157em",
+    "4.209em",
+    "5.61em",
+    "7.478em",
+    "9.969em",
+    "13.288em",
+  ],
 }
 
 export default theme
